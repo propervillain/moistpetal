73666fe8cc7149a6e6d2d7e282296cf658a42ec9
John Cramb <jcramb@gmail.com>
2017-10-21 11:21:22 +1000

    added path package and updated Gopkg.lock

---
+++ path/path.go
@@ -0,0 +1,139 @@
+// Copyright 2017, The Moistpetal Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package path exports filepath primitives for the moistpetal framework.
+package path
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+// Global default path base.
+var defaultBase = NewBase(Locate("", ""))
+
+var (
+	// HomeSymbol represents user home directory.
+	HomeSymbol = "~"
+)
+
+// Base represents a base directory for a path tree.
+type Base struct {
+	dir string
+}
+
+// NewBase returns a new default path base.
+func NewBase(dir string) *Base {
+	return &Base{
+		dir: dir,
+	}
+}
+
+// Dir returns the filepath of the base dir.
+func (b *Base) Dir() string {
+	return b.dir
+}
+
+// SetDefault overrides the default base path.
+func SetDefault(base *Base) {
+	defaultBase = base
+}
+
+// Dir for the default path base.
+func Dir() string {
+	return defaultBase.Dir()
+}
+
+/*
+Locate selects a base directory in following order:
+ 1. Directory from Environment Variable
+ 2. Provided default directory
+ 3. Current working directory
+*/
+func Locate(pathEnv string, pathDefault string) string {
+	path := func() string {
+
+		// is the path environment variable present?
+		if p := os.Getenv(pathEnv); p != "" {
+			return p
+		}
+
+		// is default path set?
+		if pathDefault != "" {
+			return pathDefault
+		}
+
+		// use working directory as last resort
+		p, _ := os.Getwd()
+		return p
+	}()
+	return Clean(path)
+}
+
+// Expand will expand environment variables and home symbol.
+func Expand(path string) string {
+	if strings.HasPrefix(path, HomeSymbol) {
+		if home := HomeDir(); home != "" {
+			path = filepath.Join(home, path[len(HomeSymbol):])
+		}
+	}
+	return os.ExpandEnv(path)
+}
+
+// Clean canonicalize's a path by expanding home symbols and
+// environment variables, evaluating symlinks and converting
+// to an absolute path.
+func Clean(path string) string {
+	path = Expand(path)
+	if p, err := filepath.EvalSymlinks(path); err == nil {
+		path = p
+	}
+	if p, err := filepath.Abs(path); err == nil {
+		path = p
+	}
+	return path
+}
+
+// HomeDir returns the current user's home directory.
+func HomeDir() string {
+
+	// macOS, linux
+	if s := os.Getenv("HOME"); s != "" {
+		return s
+	}
+
+	// windows (preferred)
+	if s := os.Getenv("USERPROFILE"); s != "" {
+		return s
+	}
+
+	// windows
+	if s := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH"); s != "" {
+		return s
+	}
+
+	return ""
+}
+
+// ProgramDir returns the program directory for the current OS.
+func ProgramDir() string {
+	switch runtime.GOOS {
+	case "windows":
+		return os.ExpandEnv("%PROGRAMDATA%")
+	default:
+		return "/usr/local"
+	}
+}
---
+++ path/path_test.go
@@ -0,0 +1,107 @@
+// Copyright 2017, The Moistpetal Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package path_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/propervillain/moistpetal/path"
+)
+
+// Example demonstrates basic usage of the path package.
+func Example() {
+
+	// configure path vars
+	pathEnv := "MOISTPETAL_PATH"
+	pathDefault := filepath.Join(path.ProgramDir(), "moistpetal")
+
+	// locate path base directory
+	path.SetDefault(path.NewBase(path.Locate(pathEnv, pathDefault)))
+
+	// open file in program directory
+	filename := filepath.Join(path.Dir(), "moistpetal.yml")
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	// do something with data
+	_ = data
+}
+
+// TestExpand tests expansion of home symbol and env vars.
+func TestExpand(t *testing.T) {
+
+	home := path.Expand(path.HomeSymbol)
+	if home == path.HomeSymbol || home == "" {
+		t.Errorf("failed to expand home symbol")
+	}
+
+	v := "wubba lubba dub dub!"
+	os.Setenv("TEST_VAR", v)
+	check(t, path.Expand("$TEST_VAR"), v, "expand environment var")
+}
+
+// TestLocate tests the various methods of finding the base path.
+func TestLocate(t *testing.T) {
+
+	pathEnv := "TEST_PATH"
+	pathDir := "testdata"
+	pathDirClean := path.Clean(pathDir)
+
+	os.Unsetenv(pathEnv)
+	base := path.NewBase(path.Locate(pathEnv, pathDir))
+	check(t, base.Dir(), pathDirClean, "set base with default dir")
+
+	os.Setenv(pathEnv, pathDir)
+	base = path.NewBase(path.Locate(pathEnv, pathDir))
+	check(t, base.Dir(), pathDirClean, "set base with default dir (empty env)")
+
+	os.Setenv(pathEnv, "testdata")
+	base = path.NewBase(path.Locate(pathEnv, ""))
+	check(t, base.Dir(), pathDirClean, "set base with environment var")
+}
+
+// TestClean tests path canonicalization.
+func TestClean(t *testing.T) {
+	var p string
+
+	p = path.Clean("~/")
+	if p == "" || p != path.HomeDir() {
+		t.Errorf("failed to clean home symbol path")
+	}
+
+	p = path.Clean("testdata")
+	cwd, _ := os.Getwd()
+	if p == "" || p != filepath.Join(cwd, "testdata") {
+		t.Errorf("failed to clean relative path")
+	}
+
+	p = path.Clean(path.ProgramDir())
+	if p == "" || p != path.ProgramDir() {
+		t.Errorf("failed to clean absolute path")
+	}
+
+	// add symbolic link test
+}
+
+func check(t *testing.T, actual string, expected string, desc string) {
+	if actual != expected {
+		t.Errorf("[%s]\ngot:\n  %q\nwant:\n  %q", desc, actual, expected)
+	}
+}
